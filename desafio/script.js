819196230a96e11cb05396f053c27a4c5909dc2b
higorsilveira <higorsilveira.hs@gmail.com>
2021-06-13 23:57:05 -0400

    Html_Css_Js completo
    Faltando apenas finalizar o relatório

---
+++ desafio/script.js
@@ -0,0 +1,43 @@
+const image = [
+    document.getElementById('img1'),
+    document.getElementById('img2'),
+    document.getElementById('img3'),
+    document.getElementById('img4')
+];
+const charName = [
+    document.getElementById('name1'),
+    document.getElementById('name2'),
+    document.getElementById('name3'),
+    document.getElementById('name4')
+]
+
+
+const numeroMaximoDePersonagens = 671;
+
+
+function initPage(){
+    for(i = 0; i < 4; i++){
+        getCharacter(i)
+    }
+}
+
+function generateRandomNumber(){
+    return Math.floor(Math.random() * numeroMaximoDePersonagens);
+}
+
+function getCharacter(i){
+    id = generateRandomNumber();
+    return fetch(`https://rickandmortyapi.com/api/character/${id}`, {
+    method: 'GET',
+    headers: {
+        Accept: 'application/json',
+        "Content-Type": 'application/json'
+    }
+    }).then((response) => response.json()).then((data) => {
+    image[i].src = data.image;
+    image[i].alt = data.name;
+    charName[i].innerHTML = data.name;
+    })
+}
+
+window.onload = initPage;
